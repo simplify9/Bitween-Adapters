c54084362c6314055c49ae79ac7912a2f47bf564
raedmazahreh <raed@simplify9.com>
2025-09-18 11:15:05 +0300

    update response encoding handling and improve file metadata management

--- SW.InfolinkAdapters.Receivers.S3/Handler.cs
+++ SW.InfolinkAdapters.Receivers.S3/Handler.cs
@@ -4,11 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using SW.CloudFiles;
 using SW.PrimitiveTypes;
 using SW.Serverless.Sdk;
-
 
 namespace SW.InfolinkAdapters.Receivers.S3
 {
@@ -23,7 +21,7 @@ namespace SW.InfolinkAdapters.Receivers.S3
             Runner.Expect(CommonProperties.TargetPath);
             Runner.Expect(CommonProperties.FolderName, "");
             Runner.Expect(CommonProperties.BatchSize, "50");
-            Runner.Expect(CommonProperties.ContentType, "base64");
+            Runner.Expect(CommonProperties.ResponseEncoding, "utf8");
             Runner.Expect(CommonProperties.DeleteMovesFileTo, null);
         }
         public async Task Initialize()
@@ -54,6 +52,7 @@ namespace SW.InfolinkAdapters.Receivers.S3
         public async Task<XchangeFile> GetFile(string fileId)
         {
             var file = await cloudFiles.OpenReadAsync(fileId);
+            var fileName = Path.GetFileName(fileId);
             byte[] bytes;
             await using (var memoryStream = new MemoryStream())
             {
@@ -61,11 +60,11 @@ namespace SW.InfolinkAdapters.Receivers.S3
                 bytes = memoryStream.ToArray();
             }
             string base64 = Convert.ToBase64String(bytes);
-            XchangeFile xchangeFile;
-            xchangeFile = Runner.StartupValueOf(CommonProperties.ContentType) switch
+            XchangeFile xchangeFile = Runner.StartupValueOf("ResponseEncoding").ToLower() switch
             {
-                "text/plain" => new XchangeFile(await new StreamReader(file).ReadToEndAsync(), fileId),
-                _ => new XchangeFile(base64, fileId)
+                "utf8" => new XchangeFile(Encoding.UTF8.GetString(bytes), fileName),
+                "base64" => new XchangeFile(base64, fileName),
+                _ => throw new ArgumentException($"Unknown ResponseEncoding: {Runner.StartupValueOf("ResponseEncoding")}. Supported: utf8, base64")
             };
 
             return xchangeFile;
@@ -76,8 +75,20 @@ namespace SW.InfolinkAdapters.Receivers.S3
             if (Runner.StartupValueOf(CommonProperties.DeleteMovesFileTo) != null)
             {
                 var file = await cloudFiles.OpenReadAsync(fileId);
-                await using var newFile = File.Create(Runner.StartupValueOf(CommonProperties.DeleteMovesFileTo));
-                await file.CopyToAsync(newFile);
+                var metadata = await cloudFiles.GetMetadataAsync(fileId);
+                var contentType = metadata["ContentType"];
+                var fileName = Path.GetFileName(fileId);
+                var key = $"{Runner.StartupValueOf(CommonProperties.DeleteMovesFileTo)}/{fileName}";
+                await using (var memoryStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(memoryStream);
+                    await cloudFiles.WriteAsync(memoryStream, new WriteFileSettings
+                    {
+                        Key = key,
+                        ContentType = contentType,
+                        Public = false
+                    });
+                }
                 await cloudFiles.DeleteAsync(fileId);
             }
         }
